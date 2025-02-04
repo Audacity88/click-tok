3e65c2af212bf1479b65b78477959827723f7a4c
Audacity88 <dfgilles@uchicago.edu>
2025-02-04 10:11:33 -0600

    Initial Commit

---
+++ clicktokTests/clicktokTests.swift
@@ -0,0 +1,17 @@
+//
+//  clicktokTests.swift
+//  clicktokTests
+//
+//  Created by Daniel Gilles on 2/4/25.
+//
+
+import Testing
+@testable import clicktok
+
+struct clicktokTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
